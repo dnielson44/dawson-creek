8670d9040f278eb84cd7ac8d6f5abe82b7d3bf08
dnielson44 <d00290186@dmail.dixie.edu>
2021-07-02 12:37:47 -0600

    added server functions

---
+++ persist.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+// const { connect } = require("./server");
+
+function connect(callback){
+    let connectionString = `mongodb+srv://DawsonNielson:Dawsonp44@cluster0.08nxx.mongodb.net/forum?retryWrites=true&w=majority`;
+    console.log("connet to db...");
+
+    mongoose
+        .connect(connectionString,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+}
+
+function onConnect(callback){
+    mongoose.connection.once("open",callback);
+}
+
+module.exports = {
+    connect,
+    onConnect
+}
